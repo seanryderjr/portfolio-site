bbe78f6ecb1e06efbd505b03ee5ef1bec85ac728
Sean Ryder <sean.ryderjr@gmail.com>
2023-04-09 23:06:56 -0400

    wdcwdc

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+
+
+const home = document.querySelector('.jumbotron');
+
+document.querySelector('a[href="#home"]').addEventListener('click', function(e) {
+  e.preventDefault(); // prevent default link behavior
+  home.scrollIntoView({ behavior: 'smooth' }); // scroll to the About section
+});
+
 const aboutSection = document.querySelector('.about');
 
 document.querySelector('a[href="#about"]').addEventListener('click', function(e) {
   e.preventDefault(); // prevent default link behavior
   aboutSection.scrollIntoView({ behavior: 'smooth' }); // scroll to the About section
+});
+
+const contact = document.querySelector('.contact');
+
+document.querySelector('a[href="#contact"]').addEventListener('click', function(e) {
+  e.preventDefault(); // prevent default link behavior
+  contact.scrollIntoView({ behavior: 'smooth' }); // scroll to the About section
 });
 
